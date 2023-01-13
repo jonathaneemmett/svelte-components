cd518d25f4d24da928d6de076d0220e2c4d3e14e
Jonathan E. Emmett <jon@emmettmedia.com>
2023-01-13 16:48:30 -0600

    debounce in search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Search from '$lib/components/Search.svelte';
+	let search = '';
+</script>
+
+<section class="search">
+	<div class="search__wrapper">
+		<Search {search} />
+	</div>
+</section>
+
+<style>
+	.search {
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.search__wrapper {
+		position: relative;
+		width: 100%;
+		max-width: 30rem;
+	}
+</style>
